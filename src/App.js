c115f8ffd65be431a4371eaa9d228f67995a1af8
yutveg <kodi.0n31l@gmail.com>
2019-11-05 15:12:34 -0800

    UI finished

--- src/App.js
+++ src/App.js
@@ -22,10 +22,16 @@ function App() {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
-        <Display />
+        <Display props="nan"/>
         <Specials />
-        <Numbers />
-        <Operators />
+        <div className="numOps">
+            <div>
+                <Numbers />
+            </div>
+            <div>
+                <Operators />
+            </div>
+        </div>
       </div>
     </div>
   );
--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Display = (props) => {
-  return <div className="display">{props}</div>;
+  return (<div className="display">NaN</div>);
 };
 
 export default Display;
